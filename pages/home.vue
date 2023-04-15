64dd3fa5af760202da38d4302a2c7f0ce9dbf16e
fryheid <fryheid@protonmail.com>
2023-04-15 20:48:07 +0200

    Restructured home screen

--- pages/home.vue
+++ pages/home.vue
@@ -1,30 +1,16 @@
 <template>
   <h1>Welcome {{ npub }}</h1>
-  <input type="file" accept=".epub" @change="changeFile($event)" />
+  <h2>Your books</h2>
+  <ul v-if="books">
+    <li v-for="book in books">{{ book.title }}</li>
+  </ul>
+  <p v-else>No books added yet</p>
+  <NuxtLink to="/books/new">
+    <button>Add book</button>
+  </NuxtLink>
 </template>
 
 <script setup lang="ts">
-import TurndownService from "turndown";
-import { Epub } from "@gxl/epub-parser/lib/parseEpub";
 const npub = useState("npub");
-
-async function changeFile(event: Event) {
-  const inputFile = event.target.files[0];
-  console.log(inputFile);
-  const reader = new FileReader();
-  reader.readAsBinaryString(inputFile);
-  await new Promise((resolve) => (reader.onload = () => resolve()));
-  const binary = reader.result;
-  const response: Epub = await $fetch("/api/parse", {
-    method: "POST",
-    body: JSON.stringify({
-      input: binary,
-    }),
-  });
-  console.log(response);
-  console.log(response.sections![7].htmlString);
-  const turndownService = new TurndownService();
-  const markdown = turndownService.turndown(response.sections![7].htmlString);
-  console.log(markdown);
-}
+const books = ref();
 </script>
